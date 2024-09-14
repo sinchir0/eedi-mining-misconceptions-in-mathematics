db58250080fab0471118a34c5de19336b7b915d2
root <dekunattou@gmail.com>
2024-09-14 02:15:35 +0000

    Initial commit

---
+++ shell/download_competition_data.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+kaggle competitions download eedi-mining-misconceptions-in-mathematics -p data
+unzip data/eedi-mining-misconceptions-in-mathematics.zip -d data
---
+++ shell/multiple_run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./shell/do_notebook_by_cli.sh eedi-mining-misconceptions-in-mathematics/exp/exp001.ipynb
