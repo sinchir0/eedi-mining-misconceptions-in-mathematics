6a46745670a9d9dbac8e7d060b3311d08a4d0917
root <dekunattou@gmail.com>
2024-10-14 23:59:25 +0000

    add

--- shell/multiple_run.sh
+++ shell/multiple_run.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# ./shell/do_notebook_by_cli.sh exp/retriever/e008-ret.ipynb
-# ./shell/do_notebook_by_cli.sh exp/retriever/e009-ret.ipynb
-./shell/do_notebook_by_cli.sh exp/retriever/e010-ret.ipynb
-# ./shell/do_notebook_by_cli.sh exp/retriever/e011-ret.ipynb
-./shell/do_notebook_by_cli.sh exp/reranker/e004-rerank-bge.ipynb
+./shell/do_notebook_by_cli.sh exp/retriever/e013-ret.ipynb
+./shell/do_notebook_by_cli.sh exp/causal/e018-causal.ipynb
