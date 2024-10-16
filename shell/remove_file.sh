839eccae0015daa13a14dc40e307dc8179c301b8
root <dekunattou@gmail.com>
2024-10-16 23:11:19 +0000

    add

---
+++ shell/remove_file.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+rm -rf ~/.cache
+rm -rf exp/causal/_unsloth_sentencepiece_temp
+rm -rf exp/causal/_unsloth_temporary_saved_buffers
+rm -rf exp/causal/aws
+rm -rf exp/causal/wandb
+rm -rf exp/causal/awscliv2.zip
+rm -rf exp/retriever/wandb
+rm -rf output
+rm -rf wandb
+rm -rf awscliv2.zip
