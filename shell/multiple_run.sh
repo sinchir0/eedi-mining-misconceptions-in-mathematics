d7146036d37e9f1c1eb09297dd030c9d1266e906
root <dekunattou@gmail.com>
2024-10-31 23:45:50 +0000

    add

--- shell/multiple_run.sh
+++ shell/multiple_run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-./shell/do_notebook_by_cli.sh exp/causal/e018-causal-openmath.ipynb
-./shell/do_notebook_by_cli.sh exp/causal/e020-causal.ipynb
+# ./shell/do_notebook_by_cli.sh exp/causal/e026-causal.ipynb
+./shell/do_notebook_by_cli.sh exp/causal/e027-causal.ipynb
+# ./shell/do_notebook_by_cli.sh exp/generate_data/e002-generate.ipynb
